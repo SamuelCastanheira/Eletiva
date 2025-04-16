c676c2e4f90c80e1b700e50ad0995681affbd06e
Samuel Castanheira <samuel.rocha.castan@gmail.com>
2025-04-16 01:35:46 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed: 	new file:   Recursao1/Combinacao.cpp

---
+++ Recursao1/Combinacao.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long long int Combinacao(int n, int p){
+  if (p==0||(n-p)==0){
+    return 1;
+  }
+  return Combinacao(n-1,p-1)*(n)/p;
+}
+
+void Funcao(){
+  int n,p;
+  cin >> n;
+  cin >> p;
+  cout << Combinacao(n,p) <<endl;
+}
+
+ 
+int main() {
+    int iteracao=0;
+    int iteracaoMaxima;
+    cin >> iteracaoMaxima;
+    do {
+        Funcao();
+        iteracao++;
+    } while (iteracao < iteracaoMaxima);
+    return 0;
+}
